4e25dc3f866cf91c479d4824014abcb45fd1be8a
Hitjunior001 <juninhorf8@hotmail.com>
2023-06-24 22:33:27 -0300

    azures

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,4 +6,8 @@ const pagamentosRouter = require('./routes/pagamentos');
 app.use('/api/reservas', reservasRouter);
 app.use('/api/pagamentos', pagamentosRouter);
 
+app.get('/', (req, res) => {
+    res.send('Hello, Azure Web App!');
+  });
+
 module.exports = app;
