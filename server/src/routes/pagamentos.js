be7e5f432832f09e8d890e4020f16e39a0af3fa4
Hitjunior001 <juninhorf8@hotmail.com>
2023-06-25 08:29:21 -0300

    newPayament

--- server/src/routes/pagamentos.js
+++ server/src/routes/pagamentos.js
@@ -1,35 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')('process.env.STRIPE_API_KEY');
 
 router.post('/', async (req, res) => {
-  try {
-    const { token, amount, user, vaga } = req.body;
-
-    // Criar uma cobrança no Stripe
-    const charge = await stripe.charges.create({
-      amount: amount,
-      currency: 'BRL',
-      source: token,
-      description: 'Pagamento pela reserva de vaga',
-    });
-
-    // Verificar se o pagamento foi aceito
-    if (charge.status === 'succeeded') {
-      // Criar a reserva e vincular ao usuário e à vaga
-    //   const reserva = await criarReserva(user, vaga);
-
-      // Fechar a vaga
-    //   await fecharVaga(vaga);
-
-      res.status(200).json({ message: 'Pagamento realizado com sucesso', reserva });
-    } else {
-      res.status(400).json({ message: 'O pagamento não foi aceito' });
-    }
-  } catch (error) {
-    console.error('Erro ao processar o pagamento:', error);
-    res.status(500).json({ message: `Erro ao processar o pagamento: ${error}` });
-  }
+ 
 });
 
 // Função para criar a reserva e vincular ao usuário e à vaga
