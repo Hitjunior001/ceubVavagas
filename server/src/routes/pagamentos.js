c23b1899555caaf1855cea33603cddc9a52963f3
Hitjunior001 <juninhorf8@hotmail.com>
2023-06-25 01:49:14 -0300

    payaments

--- server/src/routes/pagamentos.js
+++ server/src/routes/pagamentos.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const stripe = require('stripe')('process.env.STRIPE_API_KEY');
 
-router.post('/', (req, res) => {
-  console.log('Endpoint de pagamentos');
-  res.send('Endpoint de pagamentos');
+router.post('/', async (req, res) => {
+  try {
+    const { token, amount, user, vaga } = req.body;
+
+    // Criar uma cobrança no Stripe
+    const charge = await stripe.charges.create({
+      amount: amount,
+      currency: 'BRL',
+      source: token,
+      description: 'Pagamento pela reserva de vaga',
+    });
+
+    // Verificar se o pagamento foi aceito
+    if (charge.status === 'succeeded') {
+      // Criar a reserva e vincular ao usuário e à vaga
+    //   const reserva = await criarReserva(user, vaga);
+
+      // Fechar a vaga
+    //   await fecharVaga(vaga);
+
+      res.status(200).json({ message: 'Pagamento realizado com sucesso', reserva });
+    } else {
+      res.status(400).json({ message: 'O pagamento não foi aceito' });
+    }
+  } catch (error) {
+    console.error('Erro ao processar o pagamento:', error);
+    res.status(500).json({ message: 'Erro ao processar o pagamento' });
+  }
 });
+
+// Função para criar a reserva e vincular ao usuário e à vaga
+// async function criarReserva(user, vaga) {
+  // Implemente a lógica para criar a reserva e vincular ao usuário e à vaga
+  // ...
+  // Retorne a reserva criada
+// }
+
+// Função para fechar a vaga
+// async function fecharVaga(vaga) {
+  // Implemente a lógica para fechar a vaga
+  // ...
+// }
 
 module.exports = router;
--- server/src/routes/reservas.js
+++ server/src/routes/reservas.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 require('dotenv').config();
-const stripe = require('stripe')('process.env.STRIPE_API_KEY');
-
-
 
 router.post('/', (req, res) => {
   console.log('reservas')
