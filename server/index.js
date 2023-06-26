0a595bd03b2e4d4cd92d35293d2f7915e78b351f
Hitjunior001 <juninhorf8@hotmail.com>
2023-06-26 00:56:29 -0300

    request-kotlin

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const routes = require('./src/routes');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+
 
 app.use(routes);
 
 app.get('/', (req, res) => {
-  res.send('Hello, Azure Web App!');
+  const { diaAlugado, idUsuario } = req.body;
+
+  // Lógica para processar a reserva
+
+  // Exemplo de resposta de sucesso
+  res.json({ success: true });
 });
 
 const port = process.env.PORT || 8080;
