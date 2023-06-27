4c1651a05af9a9d66527cbe8576558e0aff8bb9a
Hitjunior001 <juninhorf8@hotmail.com>
2023-06-27 00:02:16 -0300

    api/reservas

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const routes = require('./src/routes');
 const bodyParser = require('body-parser');
+const admin = require("firebase-admin");
 
 const app = express();
 app.use(bodyParser.json());
 
+// Inicialize o SDK com as credenciais de serviço
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Agora você pode acessar o Firestore como administrador
+const db = admin.firestore();
 
 
 app.use(routes);
--- server/src/routes/reservas.js
+++ server/src/routes/reservas.js
@@ -3,13 +3,31 @@ const router = express.Router();
 
 require('dotenv').config();
 
-router.post('/', (req, res) => {
-  const { diaAlugado, idUsuario } = req.body;
+// Obtenha uma referência para o Firestore
+const db = admin.firestore();
 
-  // Lógica para processar a reserva
+// Defina o endpoint para receber as informações e criar a reserva
+app.post('/api/reservas', (req, res) => {
+  // Obtenha os dados enviados pelo Kotlin a partir do corpo da requisição
+  const { vagaId, userId, dataReserva } = req.body;
 
-  // Exemplo de resposta de sucesso
-  res.json({ success: true });
+  // Crie um documento no Firestore com as informações da reserva
+  db.collection('reservas')
+    .add({
+      vagaId,
+      userId,
+      dataReserva
+    })
+    .then((docRef) => {
+      // A reserva foi criada com sucesso
+      console.log('Reserva criada:', docRef.id);
+      res.status(201).json({ message: 'Reserva criada com sucesso' });
+    })
+    .catch((error) => {
+      // Ocorreu um erro ao criar a reserva
+      console.error('Erro ao criar reserva:', error);
+      res.status(500).json({ error: 'Ocorreu um erro ao criar a reserva' });
+    });
 });
 
 module.exports = router;
